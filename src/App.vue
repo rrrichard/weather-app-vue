8286d0bc9481417130cdafa900839d3e1c394b88
Richard Rabulan <richarduuu@richards-mbp.home>
2020-08-05 17:21:38 +0100

    basic functionality completed

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,27 @@
   <div id="app">
     <main>
       <div class="search-box">
-        <input type="text" class="search-bar" placeholder="Search..." />
+        <input
+          type="text"
+          class="search-bar"
+          placeholder="Search..."
+          v-model="query"
+          @keyup.enter="fetchWeather()"
+        />
       </div>
 
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+        <!-- <pre>
+        {{ weather}}
+        </pre>-->
         <div class="location-box">
-          <div class="location">Bournemouth, UK</div>
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
           <div class="date">Monday 20 January 2020</div>
         </div>
 
         <div class="weather-box">
-          <div class="temp">9°c</div>
-          <div class="weather">Rain</div>
+          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
         </div>
       </div>
     </main>
@@ -27,7 +36,26 @@ export default {
   data() {
     return {
       api_key: "a5994bb9a09a7e631a3de6ef00177d0e",
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      query: "",
+      weather: {},
     };
+  },
+  methods: {
+    fetchWeather() {
+      fetch(
+        `${this.url_base}weather?q=${this.query}&units=metric&appid=${this.api_key}`
+      )
+        .then((res) => {
+          return res.json();
+        })
+        .then(this.setResults);
+    },
+    setResults(results) {
+      this.weather = results;
+      // console.log(this.weather);
+    },
+    dateBuilder() {},
   },
 };
 </script>
